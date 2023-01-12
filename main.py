fee7da121ef6250424f3b2a43751d07b1a721ff1
Zacch-0 <122557754+Zacch-0@users.noreply.github.com>
2023-01-12 18:23:47 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+ 
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+ 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+ 
+@client.event
+async def on_message(message):
+    print("message-->", message)
+ 
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+ 
+    if message.content.startswith('image'):
+        await message.channel.send(file=discord.File('download.jpg'))
+ 
+    if message.content.startswith('video'):
+        await message.channel.send(file=discord.File('sample-mp4-file-small.mp4'))
+ 
+    if message.content.startswith('audio'):
+        await message.channel.send(file=discord.File('file_example_MP3_700KB.mp3'))
+ 
+    if message.content.startswith('file'):
+        await message.channel.send(file=discord.File('sample.pdf'))
+ 
+client.run('<Your Bot Token>')
